84535ba6a3f135d8b9b27d02e300143b8a23acac
Sam de Vitt <semen.taninn@gmail.com>
2023-11-08 20:52:59 +0200

    Some styling skatches and more

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Redirection to a notes creation page
+document.querySelector('.addButton').addEventListener('click', () => {
+    const redirectUrl = '/add-note.html';
+    window.location.href = redirectUrl;
+});
+// Adding a note
